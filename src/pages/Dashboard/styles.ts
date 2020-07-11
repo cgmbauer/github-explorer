6b672f942e4fc612ea090e038276d01fcafff99e
cgmbauer <cgmbauer@gmail.com>
2020-07-11 17:14:12 -0300

    styled errors

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -23,51 +23,47 @@ export const Form = styled.form<FormProps>`
   display: flex;
 
   input {
-  flex: 1;
+    flex: 1;
 
-  height: 70px;
+    height: 70px;
 
-  padding: 0 24px;
+    padding: 0 24px;
 
-  border: 2px solid #fff;
-  border-radius: 5px 0 0 5px;
+    border: 2px solid #fff;
+    border-radius: 5px 0 0 5px;
 
+    color: #3a3a3a;
 
-  color: #3a3a3a;
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+        border-right: 0;
+      `}
 
-  ${props =>
-    props.hasError &&
-    css`
-      border-color: #c53030;
-      border-right: 0;
-    `}
-
-  &::placeholder{
-    color: #a8a8b3;
-  }
-
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
     width: 210px;
     height: 70px;
 
-    background: #04D361;
+    background: #04d361;
 
     border-radius: 0px 5px 5px 0px;
-    border:0;
+    border: 0;
 
-    color: #FFF;
+    color: #fff;
     font-weight: bold;
 
     transition: background-color 0.3s;
 
-    &:hover{
-      background: ${shade(0.2, '#04d361')}
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
     }
   }
-
-}
 `;
 
 export const Repositories = styled.div`
