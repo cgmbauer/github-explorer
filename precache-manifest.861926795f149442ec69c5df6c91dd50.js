d553ec84916e5e2b242dde13fa9b64556a1f8038
cgmbauer <cgmbauer@gmail.com>
2020-07-12 20:45:22 -0300

    Updates

---
+++ precache-manifest.861926795f149442ec69c5df6c91dd50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8efacac8277afafa2ed10db10f4087",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "976ea0aa7f723cfd2bac",
+    "url": "/github-explorer/static/js/2.fc0d9d5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.fc0d9d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d9bd90adb81e45c5c1",
+    "url": "/github-explorer/static/js/main.2216baf4.chunk.js"
+  },
+  {
+    "revision": "48893810ad43f89df01c",
+    "url": "/github-explorer/static/js/runtime-main.91256d9f.js"
+  },
+  {
+    "revision": "fe82ec0011d3b27b10f6a2991e6ce6d2",
+    "url": "/github-explorer/static/media/github-background.fe82ec00.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
